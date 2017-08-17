8e7253fb920ccfae6e9a51aca528fe9df613a3b3
Lingxiao Wang <xwangl213@gmail.com>
2017-08-17 22:02:07 +1000

    en

--- js/main.js
+++ js/main.js
@@ -21,22 +21,27 @@ $(document).ready(function() {
   /* ev handler for see who's on the team */
   $('#fetch').on('click', function(){
     console.log("fetch!");
+    /* get input id and token */
+    groupID = $('#id-input').val();
+    myToken = $('#token-input').val();
 
-    getGroupInfo().then(function(){
-      
+    $.when( getGroupInfo() ).done(function() {
+
       for (var key in groupMembers ) {
-          var member = res[key];
+        var member = groupMembers[key];
 
-          var $row = $('<tr>').appendTo('#members-table');
+        var $row = $('<tr>').appendTo('#members-table');
 
 
-          var $avatar = $('<td>').appendTo($row)
-          var $img = $('<img>').attr('src', member.image_48).css('border-radius', '5px').appendTo($avatar);
-          var $name = $('<td>').text(member.real_name).appendTo($row);
-          var $email = $('<td>').text(member.email).appendTo($row);
+        var $avatar = $('<td>').appendTo($row)
+        var $img = $('<img>').attr('src', member.image_48).css('border-radius', '5px').appendTo($avatar);
+        var $name = $('<td>').text(member.real_name).appendTo($row);
+        var $email = $('<td>').text(member.email).appendTo($row);
 
-        }
-    })
+      }
+    });
+
+
 
     // $.getJSON('db/group_members.json').done(function(res){
     //   console.log('db file loaded');
